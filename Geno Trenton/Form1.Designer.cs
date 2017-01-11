d9190cae029726af0f52a18ca2cb22bbd8f8785d
Trenton Gailey <trenton.gailey@gmail.com>
2017-01-11 13:22:00 -0700

    Added Phenotype Ratio
    Still need to comment code

--- Geno Trenton/Form1.Designer.cs
+++ Geno Trenton/Form1.Designer.cs
@@ -37,14 +37,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(56, 100);
+            this.textBox1.Location = new System.Drawing.Point(85, 75);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(492, 20);
+            this.textBox1.Size = new System.Drawing.Size(466, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
@@ -57,21 +61,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(238, 71);
+            this.button1.Location = new System.Drawing.Point(189, 45);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(157, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generate Punnet Square";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Location = new System.Drawing.Point(9, 438);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(552, 26);
+            this.label1.Size = new System.Drawing.Size(542, 39);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.label1.Text = "Phenotype Ratio:";
             // 
             // textBox3
             // 
@@ -101,24 +105,62 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(56, 126);
+            this.textBox4.Location = new System.Drawing.Point(85, 101);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(492, 20);
+            this.textBox4.Size = new System.Drawing.Size(466, 20);
             this.textBox4.TabIndex = 7;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 171);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 127);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(552, 308);
+            this.dataGridView1.Size = new System.Drawing.Size(539, 308);
             this.dataGridView1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Dad Possibilities";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-1, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Mom Possibilities";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(9, 477);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(542, 35);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Genotype Ratio:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(104, 499);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 491);
+            this.ClientSize = new System.Drawing.Size(563, 509);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
@@ -147,6 +189,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Geno Trenton/Form1.cs
+++ Geno Trenton/Form1.cs
@@ -20,17 +20,21 @@ namespace Geno_Trenton
         private int enteredCombos;
         private double totalCombos;
 
-        DataTable data;
-        DataRow row;
+        private DataTable data;
+        private DataRow row;
+
+        private int[] phenoTypeRatios;
+        private bool[] phenotypeRatioBools;
 
         public Form1()
         {
             data = new DataTable();
             InitializeComponent();
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "";
+            label1.Text = "Phenotype Ratio: ";
             if (textBox2.Text.Length != textBox3.Text.Length)
             {
                 label1.Text = "Please Enter An Equal Amount of Alleles For Each Parent";
@@ -42,19 +46,19 @@ namespace Geno_Trenton
                 possibilitiesMom = new String[1];
                 enteredCombos = 0;
 
-                String temp = textBox2.Text;
-                enteredNum = temp.Length / 2;
+                String dadText = textBox2.Text;
+                enteredNum = dadText.Length / 2;
                 String[,] genoDad = new String[enteredNum, 2];
-                for (int seperation = 0; seperation < temp.Length; seperation++)
+                for (int seperation = 0; seperation < dadText.Length; seperation++)
                 {
-                    genoDad[seperation / 2, seperation % 2] = temp.Substring(seperation, 1);
+                    genoDad[seperation / 2, seperation % 2] = dadText.Substring(seperation, 1);
                 }
 
-                String temp2 = textBox3.Text;
+                String momText = textBox3.Text;
                 String[,] genoMom = new String[enteredNum, 2];
-                for (int seperation = 0; seperation < temp2.Length; seperation++)
+                for (int seperation = 0; seperation < momText.Length; seperation++)
                 {
-                    genoMom[seperation / 2, seperation % 2] = temp2.Substring(seperation, 1);
+                    genoMom[seperation / 2, seperation % 2] = momText.Substring(seperation, 1);
                 }
 
                 totalCombos = Math.Pow(2, enteredNum);
@@ -69,26 +73,20 @@ namespace Geno_Trenton
                 textBox4.Text = addedWord;
                 addedWord = "";
 
-                //DataTable data = new DataTable();
+
                 data.Columns.Clear();
                 data.Rows.Clear();
                 data.Clear();
 
+                phenoTypeRatios = new int[(int)Math.Pow(2,momText.Length/2)];
+                phenotypeRatioBools = new bool[(int)Math.Pow(2, momText.Length / 2)];
 
                 data.Columns.Add(" ", typeof(String));
                 for (int fatherPossibilities = 0; fatherPossibilities < possibilitiesDad.Length; fatherPossibilities++)
                 {
-                    try
-                    {
-                        //data.Columns.Add(possibilitiesDad[fatherPossibilities], typeof(String));
-                        DataColumn dc = new DataColumn();
-                        dc.DataType = typeof(String);
-                        data.Columns.Add(dc);
-                    }
-                    catch (DuplicateNameException ex)
-                    {
-
-                    }
+                    DataColumn dc = new DataColumn();
+                    dc.DataType = typeof(String);
+                    data.Columns.Add(dc);
                 }
                 row = data.NewRow();
                 row[" "] = "";
@@ -97,7 +95,7 @@ namespace Geno_Trenton
                     row[fatherPossibilities + 1] = possibilitiesDad[fatherPossibilities];
                 }
                 data.Rows.Add(row);
-                label1.Text = "";
+                label1.Text = "Phenotype Ratio: ";
                 for (int MP = 0; MP < possibilitiesMom.Length; MP++)
                 {
                     row = data.NewRow();
@@ -107,19 +105,65 @@ namespace Geno_Trenton
                         String temp3 = "";
                         for (int i = 0; i < enteredNum; i++)
                         {
-                            temp3 += possibilitiesDad[fatherPossibilities].Substring(i, 1);
-                            temp3 += possibilitiesMom[MP].Substring(i, 1);
+                            if (possibilitiesDad[fatherPossibilities].Substring(i, 1).CompareTo(possibilitiesMom[MP].Substring(i, 1)) >= 0)
+                            {
+                                temp3 += possibilitiesDad[fatherPossibilities].Substring(i, 1);
+                                temp3 += possibilitiesMom[MP].Substring(i, 1);
+                            }
+                            else
+                            {
+                                temp3 += possibilitiesMom[MP].Substring(i, 1);
+                                temp3 += possibilitiesDad[fatherPossibilities].Substring(i, 1);
+                            }
                         }
-                        //label1.Text += temp3;
-                        //textBox4.Text += possibilitiesDad.Length;
-                        //label1.Text += possibilitiesDad[fatherPossibilities];
-                        //row[possibilitiesDad[fatherPossibilities]] = label1.Text;
-                        row[fatherPossibilities + 1] = temp3;
+                        for (int phenotypes = 0; phenotypes < phenoTypeRatios.Length; phenotypes++)
+                        {
+                            phenotypeRatioBools[phenotypes] = true;
+                        }
+                        for (int phenotypes = 0; phenotypes < temp3.Length/2; phenotypes++)
+                        {
+                            if (temp3.Substring(phenotypes * 2, 1).CompareTo(temp3.Substring(phenotypes * 2, 1).ToLower()) > 0)
+                            {
+                                for (int i = 0; i < phenoTypeRatios.Length; i++)
+                                {
+                                    if ((int)((i) / (Math.Pow(2, temp3.Length / 2 - 1 - phenotypes)))%2 == 1)
+                                    {
+                                        phenotypeRatioBools[i] = false;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                for (int i = 0; i < phenoTypeRatios.Length; i++)
+                                {
+                                    if ((int)((i) / (Math.Pow(2, temp3.Length / 2 - 1 - phenotypes))) % 2 == 0)
+                                    {
+                                        phenotypeRatioBools[i] = false;
+                                    }
+                                }
+                            }
+                        }
+
+                        for (int phenotypes = 0; phenotypes < phenoTypeRatios.Length; phenotypes++)
+                        {
+                            if (phenotypeRatioBools[phenotypes])
+                            {
+                                phenoTypeRatios[phenotypes]++;
+                            }
+                        }
+                            row[fatherPossibilities + 1] = temp3;
                     }
                     data.Rows.Add(row);
                 }
                 dataGridView1.DataSource = data;
-                //data.Rows.Add("Father Possibilities", typeof(String));
+                for (int c = 0; c < phenoTypeRatios.Length; c++)
+                { 
+                    label1.Text += phenoTypeRatios[c].ToString();
+                    if (c != phenoTypeRatios.Length-1)
+                    {
+                        label1.Text += " : ";
+                    }
+                }
             }
         }
 
